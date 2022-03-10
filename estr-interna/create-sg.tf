73da850275f761d294708161fe10cd01d28ec561
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-03-10 10:59:34 +0100

    r2

--- estr-interna/create-sg.tf
+++ estr-interna/create-sg.tf
@@ -1,24 +1,23 @@
 #Creació del security group per a la public subnet                                                       
 resource "aws_security_group" "SG_public_subnet" {                                                        
-    name        = "security_group_alb"                                                                
-    description = "Allow HTTP - HTTPS"                                                                      
+    name        = "WordPress_security_group"                                                                
+    description = "Allow SSH - HTTP"                                                                      
     vpc_id      =  aws_vpc.my_vpc.id                                                                                                                                                                                
     
     ingress {                                                                                                 
-        description = "HTTP"                                                                            
-        from_port   = 80                                                                                        
-        to_port     = 80                                                                                        
+        description = "SSH from VPC"                                                                            
+        from_port   = 22                                                                                        
+        to_port     = 22                                                                                        
         protocol    = "tcp"                                                                                     
-        cidr_blocks = ["0.0.0.0/0"]                                                                         
+        cidr_blocks = ["0.0.0.0/0"]                                                                           
     }                                                                                                                                                                                                              
     
     ingress {                                                                                                  
-        description = "HTTPS"                                                                           
-        from_port   = 443                                                                                        
-        to_port     = 443                                                                                        
+        description = "HTTP from VPC"                                                                           
+        from_port   = 80                                                                                        
+        to_port     = 80                                                                                        
         protocol    = "tcp"                                                                                     
-        cidr_blocks = ["0.0.0.0/0"]
-                                                                                 
+        cidr_blocks = ["0.0.0.0/0"]                                                                           
     }                                                                                                                                                                                                               
     
     egress {                                                                                                  
@@ -32,14 +31,14 @@ resource "aws_security_group" "SG_public_subnet" {
 
 #Creació del security group per a la private subnet                                                      
 resource "aws_security_group" "SG_private_subnet_" {                                                      
-    name        = "security_group_ecs"                                                                    
-    description = "ECS"                                                                                   
+    name        = "MYSQL_security_group"                                                                    
+    description = "MYSQL"                                                                                   
     vpc_id      =  aws_vpc.my_vpc.id                                                                                                                                                                                
     
     ingress {                                                                                                 
-        description = "ecs"                                                                              
-        from_port   = aws_vpc.container_port                                                                                      
-        to_port     = aws_vpc.container_port                                                                                      
+        description = "MYSQL Port"                                                                              
+        from_port   = 3306                                                                                      
+        to_port     = 3306                                                                                      
         protocol    = "tcp"                                                                                     
         cidr_blocks = ["0.0.0.0/0"]                                                                           
     }                                                                                                                                                                                                               
@@ -50,5 +49,4 @@ resource "aws_security_group" "SG_private_subnet_" {
         protocol    = "-1"                                                                                      
         cidr_blocks = ["0.0.0.0/0"]                                                                           
     }                                                                                                     
-    
-}
+}   
